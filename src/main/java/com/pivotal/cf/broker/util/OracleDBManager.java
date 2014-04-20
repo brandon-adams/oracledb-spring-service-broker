7c135dab9fd69832c7156bb8d88c167a7303d067
brandon-adams <brandon.adams@newwave-technologies.com>
2014-04-19 21:53:50 -0400

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   build.gradle
    	modified:   src/main/java/com/pivotal/cf/broker/util/OracleDBManager.java

--- src/main/java/com/pivotal/cf/broker/util/OracleDBManager.java
+++ src/main/java/com/pivotal/cf/broker/util/OracleDBManager.java
@@ -1,10 +1,13 @@
 package com.pivotal.cf.broker.util;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import com.jcraft.jsch.*;
 
@@ -17,11 +20,10 @@ import org.springframework.stereotype.Service;
 public class OracleDBManager {
 
 	private Connection dbConn;
-	private final String dbSysUser = "SYSTEM";
+	private final String dbSysUser = "SYS";
 	private final String dbSysPass = "password";
 	private final String dbHost = "192.168.4.25";
-
-	private ProcessBuilder createDBProc;
+	
 	private JSch shell;
 	private Session session;
 	private Channel channel;
@@ -31,12 +33,25 @@ public class OracleDBManager {
 		dbConn = null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Properties props = new Properties();
+			props.put("user", dbSysUser);
+			props.put("password", dbSysPass);
+			props.put("internal_logon", "SYSDBA");
 			dbConn = DriverManager.getConnection("jdbc:oracle:thin:@" + dbHost
-					+ ":1521:", dbSysUser, dbSysPass);
+					+ ":1521:", props);
 
 			shell = new JSch();
-			shell.addIdentity("/home/opstack/heat_key.pem");
-			shell.setKnownHosts("/home/opstack/.ssh/known_hosts");
+
+			File dir = new File("util");
+
+			File[] list = dir.listFiles(new FilenameFilter() {
+				public boolean accept(File dir, String filename) {
+					return filename.endsWith(".pem");
+				}
+			});
+
+			shell.addIdentity(list[0].getAbsolutePath());
+			shell.setKnownHosts("~/.ssh/known_hosts");
 			session = shell.getSession("root", dbHost, 22);
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -52,14 +67,22 @@ public class OracleDBManager {
 		try {
 			stmnt = dbConn.createStatement();
 			// String sql = "create user " + dbName + " identified by " + pass;
+			if (!createOracleFS(dbName)){
+				throw new Exception("File system creation failed. Look into this man.");
+			}
 			String sql = createDBStmt(dbName);
 			stmnt.execute(sql);
+			stmnt.execute("$ORACLE_HOME/rdbms/admin/catalog.sql");
+			stmnt.execute("$ORACLE_HOME/rdbms/admin/catproc.sql");
+			stmnt.execute("$ORACLE_HOME/sqlplus/admin/pupbld.sql");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		}
 		System.out.println("Create success");
 		return dbName;
@@ -105,7 +128,7 @@ public class OracleDBManager {
 		Statement stmnt = null;
 		try {
 			stmnt = dbConn.createStatement();
-			String sql = "DROP USER " + dbName;
+			String sql = "DROP DATABASE " + dbName + ";";
 			stmnt.execute(sql);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -122,7 +145,7 @@ public class OracleDBManager {
 		Statement stmnt = null;
 		try {
 			stmnt = dbConn.createStatement();
-			String sql = "DROP USER " + userName + ";";
+			String sql = "DROP USER " + userName + " CASCADE;";
 			stmnt.execute(sql);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -135,41 +158,14 @@ public class OracleDBManager {
 		return userName;
 	}
 
-	private boolean createOracleFS(String dbName) {
-		try {
-			createDBProc = new ProcessBuilder("scripts/new_oracle_fs.sh",
-					dbName);
-
-			// createDBProc.e
-		} catch (Exception e) {
-			System.err.println(e.getClass().getName() + ": " + e.getMessage());
-			// System.exit(0);
-		}
-		return false;
-	}
-
-	private String createUserStmt(String userName, String userPass,
-			String dbName) {
-		StringBuilder createDB = new StringBuilder();
-		createDB.append("CREATE USER " + userName + " ")
-				.append("IDENTIFIED BY " + userPass + " ")
-				.append("DEFAULT TABLESPACE " + dbName + " ")
-				.append("TEMPORARY TABLESPACE temp ")
-				.append("PROFILE default; ")
-				.append("ALTER USER " + userName + " QUOTA UNLIMITED ON "
-						+ dbName + "; ")
-				.append("GRANT CREATE SESSION, CREATE TABLE, CREATE VIEW, CREATE SEQUENCE TO "
-						+ userName + "; ");
-
-		return null;
-	}
-
-	private String createDBStmt(String dbName) {
-
+	private boolean createDBInitFile(String dbName) {
+		boolean exit = true;
 		try {
 			session.connect();
 			channel = session.openChannel("exec");
-			((ChannelExec) channel).setCommand("./new_oracle_fs.sh");
+			// ((ChannelExec) channel).setCommand("export TEST_ENV=" + dbName +
+			// "; echo $TEST_ENV");
+			((ChannelExec) channel).setCommand("sh db_init_file.sh " + dbName);
 			channel.setInputStream(null);
 			((ChannelExec) channel).setErrStream(System.err);
 			InputStream in = channel.getInputStream();
@@ -193,22 +189,107 @@ public class OracleDBManager {
 				try {
 					Thread.sleep(1000);
 				} catch (Exception ee) {
+					System.err.println(ee.getClass().getName() + ": "
+							+ ee.getMessage());
 				}
 			}
+			if (channel.getExitStatus() != 0)
+				exit = false;
 			channel.disconnect();
 			session.disconnect();
+			//Runtime.getRuntime().exec("scp -i util/heat_key.pem -r util/scripts root@" + dbHost + ":~/");
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			exit = false;
 			// System.exit(0);
+		} finally {
+
 		}
+		return exit;
+	}
+	
+	private boolean createOracleFS(String dbName) {
+		boolean exit = true;
+		try {
+			session.connect();
+			channel = session.openChannel("exec");
+			// ((ChannelExec) channel).setCommand("export TEST_ENV=" + dbName +
+			// "; echo $TEST_ENV");
+			((ChannelExec) channel).setCommand("sh new_oracle_fs.sh " + dbName);
+			channel.setInputStream(null);
+			((ChannelExec) channel).setErrStream(System.err);
+			InputStream in = channel.getInputStream();
+			channel.connect();
+
+			byte[] tmp = new byte[1024];
+			while (true) {
+				while (in.available() > 0) {
+					int i = in.read(tmp, 0, 1024);
+					if (i < 0)
+						break;
+					System.out.print(new String(tmp, 0, i));
+				}
+				if (channel.isClosed()) {
+					if (in.available() > 0)
+						continue;
+					System.out.println("exit-status: "
+							+ channel.getExitStatus());
+					break;
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (Exception ee) {
+					System.err.println(ee.getClass().getName() + ": "
+							+ ee.getMessage());
+				}
+			}
+			if (channel.getExitStatus() != 0)
+				exit = false;
+			channel.disconnect();
+			session.disconnect();
+			//Runtime.getRuntime().exec("scp -i util/heat_key.pem -r util/scripts root@" + dbHost + ":~/");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			exit = false;
+			// System.exit(0);
+		} finally {
+
+		}
+		return exit;
+	}
+
+	private String createUserStmt(String userName, String userPass,
+			String dbName) {
+		StringBuilder createDB = new StringBuilder();
+		createDB.append("CREATE USER " + userName + " ")
+				.append("IDENTIFIED BY " + userPass + " ")
+				.append("DEFAULT TABLESPACE " + dbName + " ")
+				.append("TEMPORARY TABLESPACE temp ")
+				.append("PROFILE default; ")
+				.append("ALTER USER " + userName + " QUOTA UNLIMITED ON "
+						+ dbName + "; ")
+				.append("GRANT CREATE SESSION, CREATE TABLE, CREATE VIEW, CREATE SEQUENCE TO "
+						+ userName + "; ");
+
+		return null;
+	}
+
+	private String createDBStmt(String dbName) {
 
 		StringBuilder createDB = new StringBuilder();
-		createDB.append("CREATE DATABASE mynewdb ")
+		createDB.append("STARTUP NOMOUNT;")
+				.append("CREATE DATABASE " + dbName + " ")
 				.append("USER SYS IDENTIFIED BY " + dbSysPass + " ")
 				.append("USER SYSTEM IDENTIFIED BY " + dbSysPass + " ")
-				.append("LOGFILE GROUP 1 ('/u01/logs/my/redo01a.log','/u02/logs/my/redo01b.log') SIZE 100M BLOCKSIZE 512, ")
-				.append("GROUP 2 ('/u01/logs/my/redo02a.log','/u02/logs/my/redo02b.log') SIZE 100M BLOCKSIZE 512, ")
-				.append("GROUP 3 ('/u01/logs/my/redo03a.log','/u02/logs/my/redo03b.log') SIZE 100M BLOCKSIZE 512 ")
+				.append("LOGFILE GROUP 1 ('/u01/logs/" + dbName
+						+ "/redo01a.log','/u02/logs/" + dbName
+						+ "/redo01b.log') SIZE 100M BLOCKSIZE 512, ")
+				.append("GROUP 2 ('/u01/logs/" + dbName
+						+ "/redo02a.log','/u02/logs/" + dbName
+						+ "/redo02b.log') SIZE 100M BLOCKSIZE 512, ")
+				.append("GROUP 3 ('/u01/logs/" + dbName
+						+ "/redo03a.log','/u02/logs/" + dbName
+						+ "/redo03b.log') SIZE 100M BLOCKSIZE 512 ")
 				.append("MAXLOGHISTORY 1 ")
 				.append("MAXLOGFILES 16 ")
 				.append("MAXLOGMEMBERS 3 ")
@@ -216,28 +297,37 @@ public class OracleDBManager {
 				.append("CHARACTER SET AL32UTF8 ")
 				.append("NATIONAL CHARACTER SET AL16UTF16 ")
 				.append("EXTENT MANAGEMENT LOCAL ")
-				.append("DATAFILE '/u01/app/oracle/oradata/mynewdb/system01.dbf' ")
+				.append("DATAFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/system01.dbf' ")
 				.append("SIZE 700M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED ")
-				.append("SYSAUX DATAFILE '/u01/app/oracle/oradata/mynewdb/sysaux01.dbf' ")
+				.append("SYSAUX DATAFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/sysaux01.dbf' ")
 				.append("SIZE 550M REUSE AUTOEXTEND ON NEXT 10240K MAXSIZE UNLIMITED ")
 				.append("DEFAULT TABLESPACE users ")
-				.append("DATAFILE '/u01/app/oracle/oradata/mynewdb/users01.dbf' ")
+				.append("DATAFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/users01.dbf' ")
 				.append("SIZE 500M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED ")
 				.append("DEFAULT TEMPORARY TABLESPACE tempts1 ")
-				.append("TEMPFILE '/u01/app/oracle/oradata/mynewdb/temp01.dbf' ")
+				.append("TEMPFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/temp01.dbf' ")
 				.append("SIZE 20M REUSE AUTOEXTEND ON NEXT 640K MAXSIZE UNLIMITED ")
 				.append("UNDO TABLESPACE undotbs1 ")
-				.append("DATAFILE '/u01/app/oracle/oradata/mynewdb/undotbs01.dbf' ")
+				.append("DATAFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/undotbs01.dbf' ")
 				.append("SIZE 200M REUSE AUTOEXTEND ON NEXT 5120K MAXSIZE UNLIMITED ")
 				.append("USER_DATA TABLESPACE usertbs ")
-				.append("DATAFILE '/u01/app/oracle/oradata/mynewdb/usertbs01.dbf' ")
+				.append("DATAFILE '/u01/app/oracle/oradata/" + dbName
+						+ "/usertbs01.dbf' ")
 				.append("SIZE 200M REUSE AUTOEXTEND ON MAXSIZE UNLIMITED;");
 
-		return null;
+		return createDB.toString();
 	}
 
 	public static void main(String[] args) {
 		OracleDBManager test = new OracleDBManager();
+		//test.createOracleFS("test");
+		String result = (test.createDB("test").isEmpty() ? "ERROR" : "SUCCESS");
+		System.out.println(result);
 	}
 
 }
